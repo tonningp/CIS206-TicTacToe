525df9831d55cf6bfe7fce787aee34ed700dbd51
Paul Tonning <tonningp@users.noreply.github.com>
2017-05-30 12:18:48 -0700

    Update GameButton.java

--- GameButton.java
+++ GameButton.java
@@ -1,1 +1,14 @@
-public class
+import javax.swing.JButton;
+
+public class GameButton extends JButton {
+    private int id;
+
+    GameButton(int id) {
+        this.id = id;
+        this.setText(""+id);
+    }
+
+    public int getSquareId() {
+        return this.id;
+    }
+}
